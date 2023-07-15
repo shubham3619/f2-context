e0fb68640666c8b99329a7b305b47e0f9225daaa
shubham3619 <shubhamsharma3619@gmail.com>
2023-07-15 14:41:19 +0530

    complete project

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
 
-function PrintStudentswithMap() {
+function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(developer =>{
+    if ( developer['profession'] === 'developer' ){
+      console.log(developer);
+    }
+  });
 }
 
-function PrintStudentsbyForEach() {
+function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(developer =>{
+    if ( developer['profession'] === 'developer'){
+      console.log(developer) ;
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const add = {id:4,name:"susan",age:"20",profession:"intern"};
+  arr.push(add);
+  console.log(arr);
 }
 
-function removeFailedStudent() {
+function removeAdmin() {
   //Write your code here, just console.log
+  const update = arr.filter(item =>item.profession!="admin");
+  console.log(update);
+  arr = update; 
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newArr = [
+    {id:5,name:"harsh",age:"23",profession:"manager"},
+    {id:6,name:"priya",age:"21",profession:"CEO"}
+  ]
+  let concatenation= arr.concat(newArr);
+  console.log(concatenation);
 }
